6a770a445305baa3f99457eec5238cc326747d65
Abraham Arias <arias.abram@gmail.com>
2024-08-18 11:56:05 -0700

    modifying the way we are expecting that the element was deleted from the api and UI

--- tests/deleteDevice.spec.js
+++ tests/deleteDevice.spec.js
@@ -10,14 +10,25 @@ fixture`Delete Device`
         const lastDevice = response.data[response.data.length - 1];
         console.log(lastDevice)
 
+        // Get the 
+        const deviceElements = Selector('.device-info');
+
+        // Get the initial count of devices before deletion
+        const initialDeviceCount = await deviceElements.count;
+
         // Delete the last device via API
         await axios.delete(`http://localhost:3000/devices/${lastDevice.id}`);
     
         // Reload the page and verify the device is removed
         await t.eval(() => location.reload(true));
 
-        //Assert that the deleted device is not being displayed on the list.
-        const deletedDeviceElement = Selector('.device-info').withText(lastDevice.system_name);
+        await t.wait(2000); // waiting for the UI to reflect the change
+
+        const updatedDeviceElements = Selector('.device-info');
+
+        // Assert that the deleted device is not in the list and that the list count has decreased
+        const deletedDeviceElement = updatedDeviceElements.withText(lastDevice.system_name);
         await t
-            .expect(deletedDeviceElement.exists).notOk();
+            .expect(deletedDeviceElement.exists).notOk(`Device "${lastDevice.system_name}" should not be present in the list after deletion`)
+            .expect(updatedDeviceElements.count).eql(initialDeviceCount - 1, 'Device count should decrease by one after deletion');
     });
