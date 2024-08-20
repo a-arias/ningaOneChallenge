909578f05b419810cc0d361ec1f56aacfed2608c
Abraham Arias <arias.abram@gmail.com>
2024-08-19 23:59:30 -0700

    update renaming spec

--- tests/renameDevice.spec.js
+++ tests/renameDevice.spec.js
@@ -1,32 +1,40 @@
-import {
-    Selector
-} from 'testcafe';
-const config = require('../config/config');
-
+import {Selector} from 'testcafe';
 import axios from 'axios';
 
-fixture`Device renaming`
-    .page `${config.baseUrl}/`;
+const config = require('../config/config');
 
-test('Rename the first device and verify it', async t => {
-    // Make API call to get the list of devices
-    const response = await axios.get(config.apiUrl);
-    const firstDevice = response.data[0];
+const operatingSystems = [
+    "WINDOWS WORKSTATION",
+    "WINDOWS SERVER",
+    "MAC"
+];
 
-    // Rename the first device via API
-    await axios.put(`${config.apiUrl}/${firstDevice.id}`, {
-        "system_name": "Renamed Device1",
-        "type": "MAC",
-        "hdd_capacity": "500"
+operatingSystems.forEach(os => {
+    fixture`Device renaming`
+        .page`${config.baseUrl}/`;
+
+    test(`Rename the first device to ${os} and verify it`, async t => {
+        // Make API call to get the list of devices
+        const response = await axios.get(config.apiUrl);
+        const firstDevice = response.data[0];
+
+        // Rename the first device via API
+        await axios.put(`${config.apiUrl}/${firstDevice.id}`, {
+            "system_name": `Renamed Device ${os}`,
+            "type": os,
+            "hdd_capacity": "500"
+        });
+
+        // Reload the page
+        await t.eval(() => location.reload(true));
+
+        // Assert that the renamed device is displayed correctly in the list
+        const renamedDevice = Selector('.device-info').withText(`Renamed Device ${os}`);
+
+        await t
+            .expect(renamedDevice.find('.device-name').visible).ok()
+            .expect(renamedDevice.find('.device-name').innerText).eql(`Renamed Device ${os}`)
+            .expect(renamedDevice.find('.device-type').visible).ok()
+            .expect(renamedDevice.find('.device-type').innerText).eql(os)
     });
-
-    // Reload the page
-    await t.eval(() => location.reload(true));
-
-    //Assert that the new device is displayed correctly in the list
-    const renamedDevice = Selector('.device-info').withText('Renamed Device1');
-
-    await t
-        .expect(renamedDevice.find('.device-name').visible).ok()
-        .expect(renamedDevice.find('.device-name').innerText).eql('Renamed Device1')
 });
